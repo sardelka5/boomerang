aad6b9b2df8748a3d508e7b93b6c3965975b546a
Alisa Savorskaya <alyssen95@mail.ru>
2023-02-03 10:44:55 +0300

    created dev

--- src/Game.js
+++ src/Game.js
@@ -23,7 +23,7 @@ class Game {
   regenerateTrack() {
     // Сборка всего необходимого (герой, враг(и), оружие)
     // в единую структуру данных
-    this.track = (new Array(this.trackLength)).fill(' ');
+    this.track = new Array(this.trackLength).fill(' ');
     this.track[this.hero.position] = this.hero.skin;
   }
 
